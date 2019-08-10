d969369ef2d0eb9d07453f4b3415f30d306d9f5a
guyo <guyo@localhost.localdomain>
2019-08-10 12:14:43 +0300

    commit

---
+++ form/form.js
@@ -0,0 +1,17 @@
+var Name = document.getElementById("name");
+
+var ID = document.getElementById("id");
+
+var Phone = document.getElementById("phone");
+
+var Email = document.getElementById("email");
+
+function MyInput(){
+    if(Name, ID, Phone, Email === false){
+        alert("Invalid inputs");
+    };
+};
+
+$(document).ready(function(){
+    $
+})
